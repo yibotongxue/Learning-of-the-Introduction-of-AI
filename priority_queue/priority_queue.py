2e91b6d04fb838cd0df114126dcacdcef4ca9325
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2024-03-06 14:16:35 +0800

    Update priority_queue.py

--- priority_queue/priority_queue.py
+++ priority_queue/priority_queue.py
@@ -1,1 +1,20 @@
+import heapq
 
+class PriorityQueue :
+
+    def __init__(self) :
+        self._queue = []
+        self._index = 0
+
+    def insert(self, item, priority) : # insert item and its priority at the same time
+        heapq.heappush(self._queue, (priority, self._index, item)) # push into the heap
+        self._index += 1
+
+    def remove(self) :
+        return heapq.heappop(self._queue)[-1]
+    
+    def is_empty(self) :
+        return len(self._queue) == 0
+    
+    def front(self) :
+        return self._queue[0][-1]
