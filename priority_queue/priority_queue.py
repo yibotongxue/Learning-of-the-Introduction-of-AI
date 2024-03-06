ba9da74ea633625c87e2896e78aa4ea1ab990286
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2024-03-06 14:20:10 +0800

    Update priority_queue.py

--- priority_queue/priority_queue.py
+++ priority_queue/priority_queue.py
@@ -3,18 +3,19 @@ import heapq
 class PriorityQueue :
 
     def __init__(self) :
-        self._queue = []
-        self._index = 0
+        self._queue = [] # 堆
+        self._index = 0 # 索引
 
     def insert(self, item, priority) : # insert item and its priority at the same time
-        heapq.heappush(self._queue, (priority, self._index, item)) # push into the heap
-        self._index += 1
+        heapq.heappush(self._queue, (priority, self._index, item)) # push into the heap 
+        # 放入的是一个元组，排序是根据元组的第0个元素 priority ，第1个元素是索引，最后一个是真正要排序的元素或节点，排序时根据第0个元素，也就是priority排序
+        self._index += 1 # 索引，最后堆的总数
 
     def remove(self) :
-        return heapq.heappop(self._queue)[-1]
+        return heapq.heappop(self._queue)[-1] # 弹出队首的元素
     
     def is_empty(self) :
-        return len(self._queue) == 0
+        return len(self._queue) == 0 # 判断队列是否为空
     
     def front(self) :
-        return self._queue[0][-1]
+        return self._queue[0][-1] # 取出队首的元素
