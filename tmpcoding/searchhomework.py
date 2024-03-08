52c926ab45f7e90b9a1ab916f219552f8e54b9c4
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2024-03-08 12:37:54 +0800

    Update searchhomework.py

--- tmpcoding/searchhomework.py
+++ tmpcoding/searchhomework.py
@@ -1,1 +1,131 @@
+def depthFirstSearch(problem):
+    """
+    Search the deepest nodes in the search tree first.
 
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print("Start:", problem.getStartState())
+    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
+    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
+    """
+
+    "*** YOUR CODE HERE ***"
+    stack = util.Stack()
+    visited = []
+    stack.push((problem.getStartState(), []))
+    visited.append(problem.getStartState())
+
+    while not stack.isEmpty():
+        state, actions = stack.pop()
+        if problem.isGoalState(state):
+            return actions
+        for next in problem.getSuccessors(state):
+            n_state = next[0]
+            n_direction = next[1]
+            if n_state not in visited:
+                stack.push((n_state, actions + [n_direction]))
+        visited.append(state)
+
+
+
+
+def breadthFirstSearch(problem):
+    """Search the shallowest nodes in the search tree first."""
+    "*** YOUR CODE HERE ***"
+    queue = util.Queue()
+    visited = []
+    queue.push((problem.getStartState(), []))
+    visited.append(problem.getStartState())
+    while not queue.isEmpty():
+        state, actions = queue.pop()
+        if problem.isGoalState(state):
+            return actions
+        for next in problem.getSuccessors(state):
+            n_state = next[0]
+            n_direction = next[1]
+            if n_state not in visited:
+                queue.push((n_state, actions + [n_direction]))
+                visited.append(n_state)
+    
+#! 例题答案如下
+# def breadthFirstSearch(problem):
+#     """Search the shallowest nodes in the search tree first."""
+#     #python pacman.py -l mediumMaze -p SearchAgent -a fn=bfs --frameTime 0
+#     #python pacman.py -l bigMaze -p SearchAgent -a fn=bfs -z .5 --frameTime 0
+#     "*** YOUR CODE HERE ***"
+
+#     Frontier = util.Queue()
+#     Visited = []
+#     Frontier.push( (problem.getStartState(), []) )
+#     #print 'Start',problem.getStartState()
+#     Visited.append( problem.getStartState() )
+
+#     while Frontier.isEmpty() == 0:
+#         state, actions = Frontier.pop()
+#         if problem.isGoalState(state):
+#             #print 'Find Goal'
+#             return actions 
+#         for next in problem.getSuccessors(state):
+#             n_state = next[0]
+#             n_direction = next[1]
+#             if n_state not in Visited:
+                
+#                 Frontier.push( (n_state, actions + [n_direction]) )
+#                 Visited.append( n_state )
+
+def uniformCostSearch(problem):
+    """Search the node of least total cost first."""
+    "*** YOUR CODE HERE ***"
+    queue = util.PriorityQueue()
+    visited = []
+    queue.push((problem.getStartState(), []), 0)
+    visited.append(problem.getStartState())
+    while queue.isEmpty == 0:
+        state, actions = queue.pop()
+        visited.append(state)
+        if problem.isGoalState(state):
+            return actions
+        for next in problem.getSuccessors(state):
+            n_state = next[0]
+            n_direction = next[1]
+            n_cost = next[2]
+            if n_state not in visited:
+                queue.update((n_state, actions + [n_direction]), problem.getCostOfActions(actions + [n_direction]))
+
+
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+def aStarSearch(problem, heuristic=nullHeuristic):
+    """Search the node that has the lowest combined cost and heuristic first."""
+    "*** YOUR CODE HERE ***"
+    queue = util.PriorityQueue()
+    visited = []
+    queue.push((problem.getStartState(), []), 0)
+    visited.append(problem.getStartState())
+    while queue.isEmpty == 0:
+        state, actions = queue.pop()
+        visited.append(state)
+        if problem.isGoalState(state):
+            return actions
+        cost = problem.getCostOfActions(actions)
+        for next in problem.getSuccessors(state):
+            n_state = next[0]
+            n_direction = next[1]
+            n_cost = next[2]
+            if n_state not in visited:
+                queue.update((n_state, actions + [n_direction]), cost + n_cost + heuristic(n_state))
+
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
